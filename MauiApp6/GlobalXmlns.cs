65456454441f37b92b76e1966e16729efa2d3cea
4Mgr1 <4Mgr1@DESKTOP-EHD8S3P>
2025-11-25 13:00:07 +0100

    Dodaj pliki projektów.

---
+++ MauiApp6/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiApp6")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiApp6.Pages")]
